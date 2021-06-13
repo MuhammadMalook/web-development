3f7c90bcb7617966e56e35279f72252e96cb6ec9
Muhammad Malook <maharmohammadmalook@gmail.com>
2021-06-13 13:16:30 +0500

    Node practicing using custom module

---
+++ web-practice/NodeJs-practice/myModule.js
@@ -0,0 +1,6 @@
+function printDate()
+{
+    return new Date();
+}
+module.exports.printDate = printDate; // it will export the object.
+//Module.export = printDate //it will simply exports the function itself only
